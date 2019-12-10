c70be65ecd7d5eea265ed9c0ac9eab812f074bba
daddyaddy <at.adriano.at@gmail.com>
2019-12-10 15:35:00 +0100

    [REFACTOR] Create Folder Structure

---
+++ src/components/main/app/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import app from "./App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<app />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/components/main/app/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.scss";
+
+const App: React.FC = () => {
+  return <div className="app">test</div>;
+};
+
+export default App;
