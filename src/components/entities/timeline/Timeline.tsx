dc0f1b104e14109333c107add4142e8d9f4f04b8
blossomnight <50330117+blossomnight@users.noreply.github.com>
2019-12-19 16:43:39 +0100

    [FEAT] Timeline, Moments Sections

--- src/components/entities/timeline/Timeline.tsx
+++ src/components/entities/timeline/Timeline.tsx
@@ -1,35 +1,78 @@
 import React from "react";
 
-import TrackProps from '../../../core/track-props/TrackProps';
-import MomentProps from '../../../core/moment-props/MomentProps';
+//import TrackProps from '../../../core/track-props/TrackProps';
+//import MomentProps from '../../../core/moment-props/MomentProps';
 
 import TimelineMoment from '../timeline-moment/TimelineMoment';
 
 import './Timeline.scss';
 
 
-class Timeline extends React.Component{
+type Moment = {
+    name: string; 
+    color: string;
+    start: number; 
+    end: number;   
+}
 
-    track: TrackProps;
-    moments_arr: MomentProps[];
-    curr_time: number;
+type Track = {
+    length: number; 
+}
 
-    constructor(track: TrackProps, moments_arr: MomentProps[], curr_time: number){
-        super(track, moments_arr);
-        this.track = track; 
-        this.moments_arr = moments_arr;
-        this.curr_time = curr_time;
+type TimelineProps = {
+    duration: number;
+    moment: Array<Moment>;
+    currentTime: number;
+};
+
+
+class Timeline extends React.Component<TimelineProps> {
+    constructor(props: TimelineProps) {
+        super(props);
+        console.log(this.props.duration);
     }
 
-    // dlugosc przycinanego diva to dlugosc utworu 
-    // 780px to 20% dlugosci utworu  
-
+    //,                         
+   // left: -this.props.currentTime / 1000 * 28,
 
     render() {
         return (
-            <div id="timeline-container">
-                
-            </div>
+                <div className="timeline-container">
+                    <div className="timeline-full" style={{
+                        width: ((this.props.duration / 1000) *28)
+                        }}>
+                        <div className="timeline-sections-wrapper">
+                            <div className="timeline-section" id="timeline-section-1" style={{backgroundColor: 'violet'}}>
+                                <TimelineMoment
+                                    name={"Bass"}
+                                    color={"#456"}
+                                    start={28000}
+                                    end={30000}
+                                    trackDuration={this.props.duration}
+                                ></TimelineMoment>
+                            </div>
+                            <div className="timeline-section" id="timeline-section-2">
+                            <TimelineMoment
+                                    name={"Bass"}
+                                    color={"#459"}
+                                    start={40000}
+                                    end={50000}
+                                    trackDuration={this.props.duration}
+                                ></TimelineMoment>
+                            </div>
+                            <div className="timeline-section" id="timeline-section-3" style={{backgroundColor: 'gray'}}>
+
+                            </div>
+                            <div className="timeline-section" id="timeline-section-4">
+
+                            </div>
+                            <div className="timeline-section" id="timeline-section-5">
+
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
         );
     }
 }
